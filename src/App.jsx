3eb252f0c036e280259f0e70ec7cd74cac316672
Rodrigo Garcia <rodrigogarcia246@gmail.com>
2021-01-26 22:35:33 -0300

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarComponent from './components/navbar.js';
+
+function App() {
+  return (
+    <>
+      <NavbarComponent />
+      Hola Coder
+      </>
+
+
+    );
+}
+
+export default App;
