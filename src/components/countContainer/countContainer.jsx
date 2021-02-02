adda93012c2313d0e664f0e3fd2f61ba7f4c7dce
Rodrigo Garcia <rodrigogarcia246@gmail.com>
2021-02-02 07:44:30 -0300

    tuesday2

---
+++ src/components/countContainer/countContainer.jsx
@@ -0,0 +1,55 @@
+import React,{useState} from "react";
+import ItemCount from "./ItemCount";
+
+
+
+const CountContainer =({initial,stock,product_name})=>{
+const [count,setCount]=useState(initial);
+
+    const add=()=>{
+
+        if(count+1>stock){
+alert("Superaste la cantidad de items en stock");
+
+        }
+
+
+            else{
+
+        setCount(count+1);
+            }
+
+
+};
+
+
+    const sub=()=>{
+
+
+        if(count === 0)
+           alert("Debes agregar al menos un item al carrito");
+            else
+        setCount(count-1);
+
+
+
+        };
+        const onAdd =()=>{
+alert(`Agregaste ${count} al carrito`);
+
+
+        };
+
+
+
+    return (
+        <>
+
+        <ItemCount min={sub} max={add} onAdd={onAdd} count={count} product_name={product_name} />
+        </>
+
+
+    );
+
+};
+export default CountContainer;
--- src/containers/itemListContainer.jsx
+++ src/containers/itemListContainer.jsx
@@ -1,7 +1,28 @@
-const itemListContainer = ({greeting}) => {
-	return (
-		<a className="h1"> {greeting}</a>
-		)
+import ItemCount from './CountContainer';
+
+
+const ItemListContainer = ({name}) => {
+   return (
+
+       <div name="test">
+
+
+
+    <div class="p-3 mb-2 bg-dark text-white">
+        {name}
+
+
+
+           <ItemCount product_name='Volkswagen Golf' stock={5} initial={1} />
+               <ItemCount product_name='Ford Focus' stock={9} initial={1} />
+
+        </div>
+
+
+
+           </div>
+   )
 }
 
-export default itemListContainer;
+
+export default ItemListContainer;
